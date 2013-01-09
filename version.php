49654f945642e45b794c18b1d2a4aedacf7f54ad
John Syrinek <john.syrinek@gmail.com>
2013-01-09 16:54:04 -0600

    Moved files back to root directory for Moodle plugin repository requirements - confusing documentation

--- version.php
+++ version.php
@@ -6,6 +6,7 @@
  * @package    block
  */
 
+$plugin->release = '1.0';
 $plugin->version   = 2013010900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_theme_selector';      // Full name of the plugin (used for diagnostics)
